2722ef49e6aefe31f84444a52c043101de21ca14
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2021-01-02 04:43:01 +0100

    ♻️ Cleanup template

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - karolwojtasiuk.github.io',
-    title: 'karolwojtasiuk.github.io',
+    titleTemplate: 'Karol Wojtasiuk - %s',
+    title: 'Home',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
