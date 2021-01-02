c3261ebf0f971eea8daa679de31cb6cffce18c16
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2021-01-02 18:32:13 +0100

    ✨ Add basic info

---
+++ components/Command.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <div v-if="requireClick" @click="clicked = true">
+      <h2 class="clickable">
+        <code>> {{ name }}</code>
+      </h2>
+    </div>
+    <div v-else>
+      <h2>
+        <code>> {{ name }}</code>
+      </h2>
+    </div>
+
+    <div v-show="clicked || !requireClick">
+      <h2>{{ result }}</h2>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    result: String,
+    requireClick: Boolean,
+  },
+  data() {
+    return {
+      clicked: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.clickable {
+  cursor: pointer;
+}
+
+.clickable:hover {
+  color: gold;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,39 @@
 <template>
-  <v-card>
-    <v-card-title> Karol Wojtasiuk </v-card-title>
-    <v-card-text>
-      <p>Work In Progress</p>
-    </v-card-text>
-  </v-card>
+  <v-container>
+    <header class="d-flex justify-center align-center pa-5">
+      <v-avatar size="128">
+        <img src="https://via.placeholder.com/256" alt="Avatar" />
+      </v-avatar>
+      <div class="pa-5">
+        <Command name="echo $USER" result="Karol Wojtasiuk (Drakonter)" />
+      </div>
+    </header>
+    <Command
+      name="help"
+      result="Click command to execute it"
+      style="text-align: center"
+    />
+    <main class="d-flex">
+      <Command name="whatis $USER" :result="userName" requireClick />
+      <Command name="whereis $USER" :result="userLocation" requireClick />
+      <Command name="groups" :result="userGroups" requireClick />
+    </main>
+  </v-container>
 </template>
+
+<script>
+import Command from "~/components/Command.vue";
+
+export default {
+  components: {
+    Command,
+  },
+  data() {
+    return {
+      userName: "Karol Wojtasiuk (Drakonter)",
+      userLocation: "Zamość, Poland",
+      userGroups: "programmers, csharp, dotnet, vue",
+    };
+  },
+};
+</script>
