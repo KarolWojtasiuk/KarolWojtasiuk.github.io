5f0bd8c36372129d3badde110a1c6aa4215f565c
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2021-01-06 12:43:29 +0100

    ✨ Add `WIP` alert

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <v-app dark>
+    <v-alert border="top" colored-border type="error" dismissible>
+      Work In Progress
+    </v-alert>
     <v-container>
       <nuxt />
     </v-container>
